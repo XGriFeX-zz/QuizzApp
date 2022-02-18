81436181548591556c5f8248e74023d51a5fbe03
XGriFeX <timeshin0485@gmail.com>
2022-02-18 21:02:40 +0300

    add console.log in hooksfile

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,5 +1,7 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 import { AppDispatch, RootState } from "../redux/store"
 
+console.log("HELLO")
+
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
